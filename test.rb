6bdffcdcf6e32c84d7a55bf1ee7824b7c671a09e
jluong <jason.qh.luong@gmail.com>
2016-06-21 11:21:43 +0000

    Added solution for 8.1

---
+++ test.rb
@@ -0,0 +1,17 @@
+students = [
+  {name: "Dr. Hannibal Lecter", cohort: :november},
+  {name: "Darth Vader", cohort: :november},
+  {name: "Nurse Ratched", cohort: :november},
+  {name: "Michael Corleone", cohort: :november},
+  {name: "Alex DeLarge", cohort: :november},
+  {name: "The Wicked Witch of the West", cohort: :november},
+  {name: "Terminator", cohort: :november},
+  {name: "Freddy Krueger", cohort: :november},
+  {name: "The Joker", cohort: :november},
+  {name: "Joffrey Baratheon", cohort: :november},  
+  {name: "Norman Bates", cohort: :november}
+]
+
+students.each_with_index do |student, index|
+    puts "#{index+1}: #{student[:name]} (#{student[:cohort]} cohort)"
+end
