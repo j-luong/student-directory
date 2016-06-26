d66271b4c9232a43594324d186f9f7f76f3c8400
jluong <jason.qh.luong@gmail.com>
2016-06-26 02:53:31 +0000

    Refactoring several methods in directory.rb

--- directory.rb
+++ directory.rb
@@ -1,48 +1,57 @@
 require 'io/console'
 require 'date'
 
-students = [
-  {name: "Dr. Hannibal Lecter", cohort: 1, country: "United Kingdom", height: 150},
-  {name: "Darth Vader", cohort: 3, country: "United Kingdom", height: 178},
-  {name: "Nurse Ratched", cohort: 6, country: "France", height: 177},
-  {name: "Michael Corleone", cohort: 2, country: "France", height: 184},
-  {name: "Alex DeLarge", cohort: 9, country: "Germany", height: 210},
-  {name: "The Wicked Witch of the West", cohort: 10, country: "Germany", height: 185},
-  {name: "Terminator", cohort: 9, country: "Spain", height: 202},
-  {name: "Freddy Krueger", cohort: 11, country: "Spain", height: 140},
-  {name: "The Joker", cohort: 11, country: "United States", height: 158},
-  {name: "Joffrey Baratheon", cohort: 8, country: "United States", height: 169},  
-  {name: "Norman Bates", cohort: 4, country: "Sweden", height: 172}
-]
+# students = [
+#   {name: "Dr. Hannibal Lecter", cohort: 1, country: "United Kingdom", height: 150},
+#   {name: "Darth Vader", cohort: 3, country: "United Kingdom", height: 178},
+#   {name: "Nurse Ratched", cohort: 6, country: "France", height: 177},
+#   {name: "Michael Corleone", cohort: 2, country: "France", height: 184},
+#   {name: "Alex DeLarge", cohort: 9, country: "Germany", height: 210},
+#   {name: "The Wicked Witch of the West", cohort: 10, country: "Germany", height: 185},
+#   {name: "Terminator", cohort: 9, country: "Spain", height: 202},
+#   {name: "Freddy Krueger", cohort: 11, country: "Spain", height: 140},
+#   {name: "The Joker", cohort: 11, country: "United States", height: 158},
+#   {name: "Joffrey Baratheon", cohort: 8, country: "United States", height: 169},  
+#   {name: "Norman Bates", cohort: 4, country: "Sweden", height: 172}
+# ]
 
-def interactive_menu
-    students = []
-    loop do
-        
-        # 1. print the menu and ask the user what to do
-        puts "1. Input the students"
-        puts "2. Show the students"
-        puts "9. Exit" # 9 because we'll be adding more items
-        
-        # 2. read the input and save it into a variable
-        selection = gets.chomp
-        
-        # 3. do what the user has asked
-        case selection
-        when "1"
-            students = input_students(students)
-        when "2"
-            print_students(students)
-        when "9"
-            exit # this will cause the program to terminate
-        else
-            puts "I don't know what you meant, try again"
-        end
+# Global variable definitions
+@students = []
+
+def print_menu
+  puts "1. Input the students"
+  puts "2. Show the students"
+  puts "9. Exit" # 9 because we'll be adding more items  
+end
+
+def show_students
+    print_header
+    print_students
+    print_footer
+end
+
+def process(selection)
+    case selection
+    when "1"
+        input_students
+    when "2"
+        show_students
+    when "9"
+        exit
+    else
+        puts "I don't know what you mean, try again!"
     end
 end
 
-#Solution to 8.5/8.7/8.9
-def input_students(students)
+def interactive_menu
+    loop do
+        print_menu
+        process(gets.chomp)
+    end
+end
+
+# Solution to 8.5/8.7/8.9
+def input_students
     add_more = ''
 
     until add_more == 'N' do
@@ -72,16 +81,16 @@ def input_students(students)
             height = gets.chomp
         end
         
-        #add the student hash to the array
-        students << {name: name, 
+        # Add the student hash to the array
+        @students << {name: name, 
                      cohort: cohort,
                      country: country,
                      height: height}
         
-        if students.length == 1
-            puts "Now we have #{students.count} student"
+        if @students.length == 1
+            puts "Now we have #{@students.count} student"
         else
-            puts "Now we have #{students.count} students"
+            puts "Now we have #{@students.count} students"
         end
         
         add_more = ''
@@ -91,15 +100,12 @@ def input_students(students)
             add_more = add_more.upcase!
         end
     end
-    
-    #return the array of students
-    students
 end
 
-#Solution to 8.1, 8.4, 8.6, 8.8, 8.12
-def print_students(students)
-    #Making sure there are students in the list
-    if students.empty?
+# Solution to 8.1, 8.4, 8.6, 8.8, 8.12
+def print_students
+    # Making sure there are students in the list
+    if @students.empty?
         puts "There are no students!"
         return
     end
@@ -108,8 +114,8 @@ def print_students(students)
     longest_country = 0
     headers = {id: "ID", name: "Name", cohort: "Cohort", country: "Country", height: "Height"}
     
-    #Getting the longest name and country so we know how much spacing to use with pretty formatting
-    students.each do |student|
+    # Getting the longest name and country so we know how much spacing to use with pretty formatting
+    @students.each do |student|
         if student[:name].length > longest_name
             longest_name = student[:name].length
         end
@@ -118,20 +124,17 @@ def print_students(students)
         end
     end
     
-    #8.8 solution, sorting list by cohort
-    students = students.sort_by {|value| value[:cohort].to_i}
-    
-    print_header
+    # 8.8 solution, sorting list by cohort
+    @students = @students.sort_by {|value| value[:cohort].to_i}
     
     puts ": #{headers[:id].center(4)} : #{headers[:name].center(longest_name," ")} : #{headers[:cohort].center(9," ")} : #{headers[:country].center(longest_country," ")} : #{headers[:height].center(3," ")} :"
     
-    #8.1 solution, using .each_with_index
-    students.each_with_index do |student, index|
+    # 8.1 solution, using .each_with_index
+    @students.each_with_index do |student, index|
         index += 1
-        #8.6 solution, using .center for pretty formatting
+        # 8.6 solution, using .center for pretty formatting
         puts ": #{index.to_s.center(4)} : #{student[:name].to_s.center(longest_name, " ")} : #{Date::MONTHNAMES[student[:cohort].to_i].to_s.center(9, " ")} : #{student[:country].to_s.center((longest_country > 8 ? longest_country:7) , " ")} : #{student[:height].to_s.center(headers[:height].length, " ")} :"
     end
-    print_footer(students)
 end
 
 #Solution to 8.3
@@ -183,23 +186,14 @@ def print_header
     puts "-" * 100
 end
 
-def print_footer(names)
-    if names.empty?
+def print_footer
+    if @students.empty?
         puts
-    elsif names.count == 1
-        puts "Overall, we have #{names.count} great student"
+    elsif @students.count == 1
+        puts "Overall, we have #{@students.count} great student"
     else
-        puts "Overall, we have #{names.count} great students"
+        puts "Overall, we have #{@students.count} great students"
     end
 end
-
-#nothing happens until we call the methods
-# students = input_students(students)
-#print_students(students)
-# filter_students(students)
-# name_length(students, 12)
-# print_while(students)
-# print_students(students)
-# order_students(students)
 
 interactive_menu
